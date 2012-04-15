565ff13d1d129f09e33b567f095922f12c91626b
Ivonne Yáñez Mendoza <ivonne.wftm@gmail.com>
2012-04-15 17:36:41 -0300

    Initial commit. Begin trolling the world.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hello World!
+
+Hola a todo el mundo!
---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	printf("Hola mundo, vengo a trollearlos porque igual tengo Github\n");
+	printf("Yo tambien escribo 10 lineas de codigo para cerrar una ventana\n");
+	return EXIT_SUCCESS;
+}
